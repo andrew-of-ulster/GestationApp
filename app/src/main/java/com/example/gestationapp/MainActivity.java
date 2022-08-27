230ed078c1744b51baabb9d282d56ff5532a25bf
andrew-of-ulster <amullan1010@gmail.com>
2022-08-27 15:11:31 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/gestationapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.gestationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button firstButton;
+    TextView fartText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstButton = (Button) findViewById(R.id.BUTTON);
+
+        fartText = (TextView) findViewById(R.id.textView);
+
+
+
+
+
+
+        firstButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick (View view){
+                if(!fartText.getText().toString().equals("Button was clicked"))
+                fartText.setText("Button was clicked");
+                else{fartText.setText("Hello Fart!");
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+
+}
