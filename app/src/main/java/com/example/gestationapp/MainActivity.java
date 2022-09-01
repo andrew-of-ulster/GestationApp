05c60d3054dcd69ef5f5f443c0655f55ffcb5075
Matthew <matthewhwallace17@gmail.com>
2022-09-01 23:49:03 +0100

    main page is beautiful

--- app/src/main/java/com/example/gestationapp/MainActivity.java
+++ app/src/main/java/com/example/gestationapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -71,9 +72,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(newIntent);
             }
         });
-
-
-
 
     }
 
