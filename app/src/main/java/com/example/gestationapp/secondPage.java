5fe7ea0bc783b1562f2a3b6ace551389e6ba2333
andrew-of-ulster <amullan1010@gmail.com>
2022-09-01 22:45:02 +0100

    Can select Date

--- app/src/main/java/com/example/gestationapp/secondPage.java
+++ app/src/main/java/com/example/gestationapp/secondPage.java
@@ -4,11 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class secondPage extends AppCompatActivity {
     Button backToFirst;
+    TextView op;
+    CalendarView calenderClick;
+    Button getDate;
+
+
+
 
 
     @Override
@@ -16,8 +29,28 @@ public class secondPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondpage);
 
-
         backToFirst = (Button) findViewById(R.id.backButton);
+        CalendarView simpleCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        getDate = (Button) findViewById(R.id.getDate);
+        op = (TextView) findViewById(R.id.output);
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+
+        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+
+            @Override
+            public void onSelectedDayChange( CalendarView view, int year, int month, int dayOfMonth) {
+                String curDate = dayOfMonth + "/" + ++month + "/" + year;
+                op.setText(curDate);
+
+            }
+        });
+
+
+
+
 
        backToFirst.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +59,12 @@ public class secondPage extends AppCompatActivity {
                 startActivity(newIntent);
             }
         });
+
+
+
+
+
+
     }
 
 }
