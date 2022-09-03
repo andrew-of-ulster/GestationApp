9cead54f8135d851a6a6aebed88a79c85194b84a
andrew-of-ulster <amullan1010@gmail.com>
2022-09-03 22:58:15 +0100

    Added Nav bar - was painful

--- app/src/main/java/com/example/gestationapp/MainActivity.java
+++ app/src/main/java/com/example/gestationapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gestationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +11,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.gestationapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     Button firstButton,toDateBut,toScrollBut, toFrameBut, toGridBut, toNotiBut;
     TextView welcome;
@@ -18,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         welcome = (TextView) findViewById(R.id.textView);
 
@@ -81,7 +89,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.list:
+                    replaceFragment(new ListFragment());
+                    break;
+                case R.id.Calander:
+                    replaceFragment(new CalendarFragment());
+            }
+            return true;
+        });
+
+
     }
 
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_Layout, fragment);
+        fragmentTransaction.commit();
+    }
 
 }
